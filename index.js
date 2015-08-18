065373b399540cc1a75394d50523875249559ae0
david barinas <david.barinas.dev@gmail.com>
2015-08-18 06:52:46 -0500

    [+] add check word option to library

---
+++ index.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+
+var words = function(language){
+    var possibleLanguages = ['de', 'en', 'es', 'fr'];
+
+    language = language && language.toLowerCase() || 'en';
+
+    if(possibleLanguages.indexOf(language) == -1) throw new Error(language + " is not valid language");
+    return {
+        check : function(word){
+            var content = fs.readFileSync('words/'+language+'.txt');
+            var regex = new RegExp('\n' + word +'\n');
+            if ( content.toString('utf-8').match(regex)) {
+                return true;
+            }
+            return false;
+        }
+    };
+};
+
+module.exports = words;
