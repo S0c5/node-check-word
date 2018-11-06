3f86c256cc436aa22df513b92a18f9f181941a0f
James T <jamest@earny.co>
2018-11-06 12:01:22 -0800

    add: docstring

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 const escapeStringRegexp = require('escape-string-regexp');
 
+/**
+ * @param language {string} two character abbreviation for language containing words for that language;
+ * supports: de, en, es, fr
+ * @returns {{check: (function(*=): boolean)}} a function that determines if the given word is a valid word in the
+ * language
+ */
 module.exports = function words(language) {
     const possibleLanguages = ['de', 'en', 'es', 'fr'];
 
