71bc7a152230981f82b7a870174a21f7397f6224
James T <jamest@earny.co>
2018-11-06 11:57:48 -0800

    chg: use ecma6 standards

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-var fs = require('fs');
-var escapeStringRegexp = require('escape-string-regexp');
+const fs = require('fs');
+const escapeStringRegexp = require('escape-string-regexp');
 
-var words = function(language){
-    var possibleLanguages = ['de', 'en', 'es', 'fr'];
+module.exports = function words(language) {
+    const possibleLanguages = ['de', 'en', 'es', 'fr'];
 
     language = language && language.toLowerCase() || 'en';
 
     if (possibleLanguages.indexOf(language) === -1) throw new Error(language + " is not valid language");
-    var content = fs.readFileSync(__dirname + '/words/'+language+'.txt');
-    var languageWords = content.toString('utf-8');
+    const content = fs.readFileSync(__dirname + '/words/'+language+'.txt');
+    const languageWords = content.toString('utf-8');
     return {
         check : function(word) {
             // escape special regex characters to match them literally; "I got $ ?" => "I got \$ \?"
             word = escapeStringRegexp(word);
-            // var regex = new RegExp('\n' + word +'\n');
             return !!languageWords.match(word);
         }
     };
 };
-
-module.exports = words;
