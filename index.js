8ffb96f48a5961dc4c36bd8a9f5963dd631eee0f
James T <jamest@earny.co>
2018-11-07 12:59:29 -0800

    chg: use language of words as regex to test input word

--- index.js
+++ index.js
@@ -11,15 +11,12 @@ module.exports = function words(language) {
     const possibleLanguages = ['de', 'en', 'es', 'fr'];
 
     language = language && language.toLowerCase() || 'en';
+    const languageRegex = require(`./word-regexes/${language}-regex.js`);
 
     if (possibleLanguages.indexOf(language) === -1) throw new Error(language + " is not valid language");
     const content = fs.readFileSync(__dirname + '/words/'+language+'.txt');
     const languageWords = content.toString('utf-8');
     return {
-        check : function(word) {
-            // escape special regex characters to match them literally; "I got $ ?" => "I got \$ \?"
-            word = escapeStringRegexp(word);
-            return !!languageWords.match(new RegExp(word, 'i'));
-        }
+        check: word => languageRegex.test(word),
     };
 };
