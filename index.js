562f220a59bb5b67f8561b7f4d8eaca24661f092
James T <jamest@earny.co>
2018-11-07 13:01:04 -0800

    rm: unused files and code

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const fs = require('fs');
-const escapeStringRegexp = require('escape-string-regexp');
-
 /**
  * @param language {string} two character abbreviation for language containing words for that language;
  * supports: de, en, es, fr
@@ -11,11 +8,9 @@ module.exports = function words(language) {
     const possibleLanguages = ['de', 'en', 'es', 'fr'];
 
     language = language && language.toLowerCase() || 'en';
+    if (possibleLanguages.indexOf(language) === -1) throw new Error(language + " is not valid language");
     const languageRegex = require(`./word-regexes/${language}-regex.js`);
 
-    if (possibleLanguages.indexOf(language) === -1) throw new Error(language + " is not valid language");
-    const content = fs.readFileSync(__dirname + '/words/'+language+'.txt');
-    const languageWords = content.toString('utf-8');
     return {
         check: word => languageRegex.test(word),
     };
